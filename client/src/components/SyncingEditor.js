c5b1f47cfe15d3f68b8409e1db5469d8e0af3c79
Albert Gozzi <albertgozzi@gmail.com>
2020-04-28 13:50:03 -0400

    Formatted theory summary

--- client/src/components/SyncingEditor.js
+++ client/src/components/SyncingEditor.js
@@ -188,16 +188,19 @@ export const SyncingEditor = (props) => {
             />
           </Slate>
         </section>
-        <div className="theoryEditor">
-          {value.map(text => {
-            console.log(text.children);
-            return <p>{text.children.map(element => {
-              if (element.theory) {
-                return <span>{element.text}</span>;
-              }
-            })}</p>
-          })}
-        </div>
+        <section>
+          <h4 class="theoryTitle"> Theory Summary </h4>
+          <div className="theoryEditor">
+            {value.map((text,i) => {
+              return <p key={i}>{text.children.map((element, j) => {
+                if (element.theory) {
+                  return <span key={j}>{element.text}</span>;
+                }
+                return '';
+              })}</p>
+            })}
+          </div>          
+        </section>
     </div>
   );
 }
